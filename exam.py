f744673ba7c6db2a8dd4cc4d09cf1156c88f15a4
Emil Folino <emilfolino@gmail.com>
2018-10-12 12:54:10 +0200

    Initial commit with assignment for Andreas

---
+++ exam.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+Write your code in this file. Fill out the defined functions with your solutions.
+You are free to write additional functions and modules as you see fit.
+"""
+
+def analyze_text():
+    """
+    Assignment 1
+    """
+    return True
+
+def verify_hex(hex_value):
+    """
+    Assignment 2
+    """
+    return hex_value
+
+def find_duplicates(values):
+    """
+    Assignment 3
+    """
+    return values
+
+def types(items):
+    """
+    Assignment 4
+    """
+    return items
+
+def validate_email(email):
+    """
+    Assignment 5
+    """
+    return email
+
+if __name__ == '__main__':
+    analyze_text()
+    verify_hex("")
+    find_duplicates([])
+    types([])
+    validate_email("")
